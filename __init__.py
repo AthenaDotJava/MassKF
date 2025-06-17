1cc680ef96fd8f1d68c56e796ad2727a686f8d2c
StratosDerg <120200644+StratosDerg@users.noreply.github.com>
2025-06-17 04:35:47 -0500

    v1.1.0

--- __init__.py
+++ __init__.py
@@ -22,13 +22,24 @@ bl_info = {
 
 import bpy # type: ignore
 from mathutils import * # type: ignore
+ 
+def masskf(context, path, index, value=None, copy=False, active=True):
+        if active:
+            context.active_object.keyframe_insert(
+                data_path=path, index=index, frame=context.scene.frame_current
+                )
+        for obj in context.selected_objects:
+            if active or obj!=context.active_object:
+                if hasattr(obj, path):
+                    if copy==True:
+                        setattr(obj, path, value)
+                    obj.keyframe_insert(
+                        data_path=path, index=index, frame=context.scene.frame_current
+                    )
 
-D = bpy.data
-C = bpy.context
-
-class MassKeyframe(bpy.types.Operator):
-    """Duplicate a property and add a keyframe to all selected objects"""
-    bl_idname = "animation.mass_duplicate_keyframe"
+class MKFDupeAll(bpy.types.Operator):
+    """Duplicate a property from active and add a keyframe to all selected objects"""
+    bl_idname = "anim.mkf_dupe_all"
     bl_label = "Dupicate Keyframe to Selected Objects"
     bl_options = {'REGISTER', 'UNDO'}
 
@@ -37,22 +48,15 @@ class MassKeyframe(bpy.types.Operator):
             active = context.property
             block, path, index = active
             value = getattr(block, path)
-            masskf(context=context, path=path, index=index, value=value, copy=True)
-
-        return {'FINISHED'}
-    
-def masskf(context, path, index, value=None, copy=False):
-        for obj in context.selected_objects:
-            if hasattr(obj, path):
-                if copy==True:
-                    setattr(obj, path, value)
-                obj.keyframe_insert(
-                    data_path=path, index=index, frame=context.scene.frame_current
+            masskf(
+                context=context, path=path, index=index, value=value, copy=True, active=True
                 )
 
-class NoDupe(bpy.types.Operator):
-    """Add a keyframe for selected property for all selected objects"""
-    bl_idname = "animation.mass_add_keyframe"
+        return {'FINISHED'}
+   
+class MKFAddAll(bpy.types.Operator):
+    """Add a keyframe of selected property to all selected objects"""
+    bl_idname = "anim.mkf_add_all"
     bl_label = "Add Keyframe to Selected Objects"
     bl_options = {'REGISTER','UNDO'}
 
@@ -60,24 +64,81 @@ class NoDupe(bpy.types.Operator):
         if context.property:
             active = context.property
             block, path, index = active
-            masskf(context=context, path=path, index=index, copy=False)
+            masskf(
+                context=context, path=path, index=index, copy=False, active=True
+                )
         
         return {'FINISHED'}
 
+# class MKFDupeFrom(bpy.types.Operator):
+#     """Duplicate a property from active and add a keyframe to all selected objects excluding active"""
+#     bl_idname = "anim.mkf_dupe_from"
+#     bl_label = "Add Keyframe to Selected Objects"
+#     bl_options = {'REGISTER','UNDO'}
+
+#     def execute(self,context):
+#         if context.property:
+#             active = context.property
+#             block, path, index = active
+#             masskf(
+#                 context=context, path=path, index=index, copy=True, active=False
+#                 )
+        
+#         return {'FINISHED'}
+
+# class MKFAddFrom(bpy.types.Operator):
+#     """Add a keyframe for selected property to all selected objects excluding active"""
+#     bl_idname = "anim.mkf_add_from"
+#     bl_label = "Add Keyframe to Selected Objects"
+#     bl_options = {'REGISTER','UNDO'}
+
+#     def execute(self,context):
+#         if context.property:
+#             active = context.property
+#             block, path, index = active
+#             masskf(
+#                 context=context, path=path, index=index, copy=False, active=False
+#                 )
+        
+#         return {'FINISHED'}
+
 def menu_func(self,context):
     layout = self.layout
-    layout.separator()
-    self.layout.operator(MassKeyframe.bl_idname)
-    self.layout.operator(NoDupe.bl_idname)
+    if context.area.ui_type=='PROPERTIES':
+        layout.separator()
+
+        layout.menu("UI_MT_mkf_submenu")
+
+class MKFMenu(bpy.types.Menu):
+    bl_idname = "UI_MT_mkf_submenu"
+    bl_label = "Mass Keyframe"
+
+    def draw(self,context):
+        layout = self.layout
+
+        # Submenu Layout
+        layout.operator("anim.mkf_dupe_all",text="Duplicate a keyframe to Selected")
+        layout.operator("anim.mkf_add_all",text="Add a keyframe to Selected")
+        # layout.operator("anim.mkf_dupe_from",text="Duplicate a keyframe from Active")
+        # layout.operator("anim.mkf_add_from",text="Add a keyframe to from Active")
+
 
 def register():
-    bpy.utils.register_class(MassKeyframe)
-    bpy.utils.register_class(NoDupe)
+    bpy.utils.register_class(MKFDupeAll)
+    bpy.utils.register_class(MKFAddAll)
+    # bpy.utils.register_class(MKFAddFrom)
+    # bpy.utils.register_class(MKFDupeFrom)
+    bpy.utils.register_class(MKFMenu)
+    
     bpy.types.UI_MT_button_context_menu.append(menu_func)
 
 def unregister():
-    bpy.utils.unregister_class(MassKeyframe)
-    bpy.utils.unregister_class(NoDupe)
+    bpy.utils.unregister_class(MKFDupeAll)
+    bpy.utils.unregister_class(MKFAddAll)
+    # bpy.utils.unregister_class(MKFAddFrom)
+    # bpy.utils.unregister_class(MKFDupeFrom)    
+    bpy.utils.unregister_class(MKFMenu)
+    
     bpy.types.UI_MT_button_context_menu.remove(menu_func)
 
 if __name__ == "__main__":
